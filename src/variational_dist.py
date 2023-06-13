dd56ad0104b1fa4fd8a172ea69c7a47972a934b9
Eirik Aalstad Baekkelund <89584092+eirikbaekkelund@users.noreply.github.com>
2023-06-13 12:29:01 +0100

    variational dist class added

---
+++ src/variational_dist.py
@@ -0,0 +1,70 @@
+from gpytorch.variational import CholeskyVariationalDistribution, MeanFieldVariationalDistribution, UnwhitenedVariationalStrategy
+from gpytorch.variational import NaturalVariationalDistribution, TrilNaturalVariationalDistribution
+
+#############################################
+######## Variational Distribution ###########
+#############################################
+
+class VariationalBase:
+    """ 
+    Class for creating variational distributions based on specifications 
+    in the configuration dictionary to be used in the variational 
+    strategy for the GP model.
+
+    Args:
+        config (dict): configuration of the GP model
+    """
+    def __init__(self, config):
+        # check for gradient type
+        if config['type'] == 'stochastic':
+            self.variational_distribution = self.get_stochastic(config)
+        elif config['type'] == 'natural':
+            self.variational_distribution = self.get_natural(config)
+    
+    def delete_non_config(self, config):
+        """ 
+        Delete non-configuration parameters
+
+        Args:
+            config (dict): configuration of the GP model
+        """
+        del config['type']
+        del config['name']
+        
+        return config
+        
+    def get_stochastic(self, config):
+        """ 
+        Create stochastic variational distribution
+
+        Args:
+            config (dict): configuration of the GP model
+        """
+        assert config['name'] in ['cholesky', 'mean_field', 'unwhitened'], 'Variational distribution must be either cholesky, mean_field or unwhitened'
+        
+        name = config['name']
+        config = self.delete_non_config(config)
+
+        if name == 'cholesky':
+            return CholeskyVariationalDistribution(**config)
+        elif name == 'mean_field':
+            return MeanFieldVariationalDistribution(**config)
+        elif name == 'unwhitened':
+            return UnwhitenedVariationalStrategy(**config)
+    
+    def get_natural(self, config):
+        """ 
+        Create natural variational distribution
+
+        Args:
+            config (dict): configuration of the GP model
+        """
+        assert config['name'] in ['natural', 'tril_natural'], 'Variational distribution must be either natural or tril_natural'
+        
+        name = config['name']
+        config = self.delete_non_config(config)
+
+        if name == 'natural':
+            return NaturalVariationalDistribution(**config)
+        elif name == 'tril_natural':
+            return TrilNaturalVariationalDistribution(**config)
