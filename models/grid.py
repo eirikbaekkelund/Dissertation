e2ca2e367644acef511046bd16cabeb57f77dd72
Eirik Aalstad Baekkelund <89584092+eirikbaekkelund@users.noreply.github.com>
2023-08-11 01:40:08 +0100

    instantiated xgboost, still a todo

--- models/grid.py
+++
@@ -1,7 +0,0 @@
-import torch
-import gpytorch
-from gpytorch.models import ApproximateGP
-
-# TODO Implement GridGP
-class GridGPModel(ApproximateGP):
-    pass
---
+++ models/spatiotemporal.py
@@ -0,0 +1,6 @@
+# TODO create a spatiotemporal GP model with exogenous variables
+# using quasi-periodic kernel and zero mean function
+
+class SpatiotemporalGP:
+    def __init__(self):
+        pass
